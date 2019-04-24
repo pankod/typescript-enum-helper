767250efabcba6b7743833aa2cbb8892d88cf96f
aydın akyol <aydinakyol11@gmail.com>
2019-04-24 15:21:08 +0300

    npm bug fix

---
+++ src/Example.ts
@@ -0,0 +1,26 @@
+import { EnumHelper } from './Helper'
+import { Gods } from './Interfaces';
+// enum Gods {
+//     Pan,
+//     Ares,
+//     Zeus,
+//     Hermes,
+//     Apollo,
+//     Uranus,
+//     Achelous,
+//     Atlas,
+//     Castor,
+//     Chaos,
+//     Hades,
+//     Heracles,
+//     Poseidon
+// }
+
+// resultKeys will be equal to: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+const resultKeys = EnumHelper.getKeys(Gods);
+
+// resultValues  will be equal to:[ 'Pan','Ares','Zeus','Hermes','Apollo','Uranus','Achelous','Atlas','Castor','Chaos','Hades','Heracles','Poseidon' ]
+const resultValues = EnumHelper.getValues(Gods);
+
+// resultName  will be equal to: Pan
+const resultName = EnumHelper.getName(Gods, 0);
